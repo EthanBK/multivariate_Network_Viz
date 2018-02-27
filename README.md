67a1269121ec3b2c645868cffcff4aae1077b904
EthanBK <bhou@ucdavis.edu>
2018-02-27 14:19:22 -0800

    data processing, unfinished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# multivariate_Network_Viz
+# Multivariate Network Explore and Presentation
+----
+
+## Dataset
+We use [Flight Route Database](https://www.kaggle.com/open-flights/flight-route-database) that contains 59,063 flight routes around the world.
+
+----
--- src/low_level.js
+++ src/low_level.js
@@ -47,12 +47,10 @@ function low_level(selector, data) {
                     flight: [f.al_name]
                 }],
                 out_edge_dic: [f.al_des],
-                out_airlines: [f.al_name],
                 num_out_airline: 1,
                 // record in and out separately
                 in_edges: [],
                 in_edge_dic: [],
-                in_airlines: [],
                 num_in_airline: 0
             };
             airports.push(ap_temp);
@@ -64,27 +62,26 @@ function low_level(selector, data) {
             var num_ap = airports.length;
             for (var i = 0; i < num_ap; i++) {
                 // update edge or create a new edge
-                if (airports[i].al_name === f.al_src) {
-                    airports[i].airline.push(f);
-                    airports[i].num_airline += 1;
+                if (airports[i].name === f.al_src) {
+                    airports[i].num_out_airline += 1;
                     // a new edge. Create a new edge and push
                     if (airports[i].out_edge_dic.indexOf(f.al_des) < 0) {
                         // new edge
                         var edge_tmp = {
-                            link_with: f.al_des,
+                            edge_des: f.al_des,
+                            edge_desId: f.al_desID,
                             num_link: 1,
                             flight: [f.al_name]
                         };
                         airports[i].out_edges.push(edge_tmp);
                         airports[i].out_edge_dic.push(f.al_des);
                     }
-
                     // Existing edge, just need to update
                     else {
                         airports[i].out_edges.forEach(function (edge) {
-                            if (edge.link_with === f.al_des) {
+                            if (edge.edge_des === f.al_des) {
                                 edge.num_link += 1;
-                                edge.flight.push(f.airline);
+                                edge.flight.push(f.al_name);
                             }
                         });
                     }
@@ -94,20 +91,21 @@ function low_level(selector, data) {
         }
 
         // build or update al_des airport, basic the same for src airport
-        if (airport_dic.indexOf(f.al_desID) < 0) {
+        if (airport_dic.indexOf(f.al_des) < 0) {
             ap_temp = {
-                al_name: f.al_des,
-                al_id: f.al_desID,
+                name: f.al_des,
+                id: f.al_desID,
                 out_edges: [],
                 out_edge_dic: [],
+                num_out_airline: 0,
                 in_edges: [{
-                    link_with: f.al_des,
+                    edge_src: f.al_src,
+                    edge_srcId: f.al_srcID,
                     num_link: 1,
-                    flight: [f.airline]
+                    flight: [f.al_name]
                 }],
                 in_edge_dic: [f.al_src],
-                airlines: [f.airline],
-                num_airline: 1
+                num_in_airline: 1
             };
             airports.push(ap_temp);
             airport_dic.push(f.al_des);
@@ -115,42 +113,43 @@ function low_level(selector, data) {
         else {
             num_ap = airports.length;
             for (i = 0; i < num_ap; i++) {
-                airports[i].in_airlines.push(f);
-                airports[i].num_in_airline += 1;
                 // update edge or create a new edge
-                if (ap === f.al_des) {
+                if (airports[i].name === f.al_des) {
+                    airports[i].num_in_airline += 1;
                     // a new edge. Create a new edge and push
-                    if (ap.in_edge_dic.indexOf(f.al_src) < 0) {
+                    if (airports[i].in_edge_dic.indexOf(f.al_src) < 0) {
                         // new edge
                         edge_tmp = {
-                            link_with: f.al_src,
+                            edge_src: f.al_src,
+                            edge_srcId: f.al_srcID,
                             num_link: 1,
-                            flight: [f.airline]
+                            flight: [f.al_name]
                         };
-                        ap.in_edges.push(edge_tmp);
-                        ap.in_edge_dic.push(f.al_des);
+                        airports[i].in_edges.push(edge_tmp);
+                        airports[i].in_edge_dic.push(f.al_des);
                     }
 
                     // Existing edge, just need to update
                     else {
-                        ap.in_edges.forEach(function (edge) {
-                            if (edge.link_with === f.src) {
+                        airports[i].in_edges.forEach(function (edge) {
+                            if (edge.edge_src === f.src) {
                                 edge.num_link += 1;
-                                edge.flight.push(f.airline);
+                                edge.flight.push(f.al_name);
                             }
                         });
                     }
+                    break;
                 }
             }
         }
     });
     // console.log('flight', flights);
+    airports.sort(function (a, b) {
+        if (isNaN(a.id)) return -1;
+        if (isNaN(b.id)) return 1;
+        else {
+            return a.id - b.id;
+        }
+    });
     console.log('airport', airports);
-    // airport_dic.sort(function (a, b) {
-    //     if (isNaN(a)) return -1;
-    //     if (isNaN(b)) return 1;
-    //     else {
-    //         return a - b;
-    //     }
-    // });
 }
