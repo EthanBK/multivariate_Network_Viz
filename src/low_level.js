9037009a849c0b75beaff46c6e3d4d0b779d2d70
EthanBK <bhou@ucdavis.edu>
2018-03-01 00:02:14 -0800

    draw all links

--- src/low_level.js
+++ src/low_level.js
@@ -1,16 +1,17 @@
 // Process data
 d3.queue()
-    .defer(d3.csv, 'data/routes.csv')
+    .defer(d3.csv, 'data/test_data.csv')
     .defer(d3.csv, 'data/airport_supplement.csv')
-    .await(function (error, flight, ap_supt){
+    .defer(d3.csv, 'data/all_airports.csv')
+    .await(function (error, flight, ap_supt, all_ap){
         if (error) {
             console.log(error)
         }
-        low_level('#low_level', flight, ap_supt)
+        low_level('#low_level', flight, ap_supt, all_ap)
     });
 
 // Build flights data
-function BuildData(flight, ap_supplement) {
+function BuildData(flight, ap_supplement, all_ap) {
     var flights,
         airports = [],
         airport_dic = [];
@@ -44,7 +45,6 @@ function BuildData(flight, ap_supplement) {
                 out_edges: [{
                     edge_des: f.al_des,
                     edge_desId: f.al_desID,
-                    num_link: 1,
                     flight: [f.al_name]
                 }],
                 out_edge_dic: [f.al_des],
@@ -71,7 +71,6 @@ function BuildData(flight, ap_supplement) {
                         var edge_tmp = {
                             edge_des: f.al_des,
                             edge_desId: f.al_desID,
-                            num_link: 1,
                             flight: [f.al_name]
                         };
                         airports[i].out_edges.push(edge_tmp);
@@ -81,7 +80,6 @@ function BuildData(flight, ap_supplement) {
                     else {
                         airports[i].out_edges.forEach(function (edge) {
                             if (edge.edge_des === f.al_des) {
-                                edge.num_link += 1;
                                 edge.flight.push(f.al_name);
                             }
                         });
@@ -102,7 +100,6 @@ function BuildData(flight, ap_supplement) {
                 in_edges: [{
                     edge_src: f.al_src,
                     edge_srcId: f.al_srcID,
-                    num_link: 1,
                     flight: [f.al_name]
                 }],
                 in_edge_dic: [f.al_src],
@@ -123,18 +120,16 @@ function BuildData(flight, ap_supplement) {
                         edge_tmp = {
                             edge_src: f.al_src,
                             edge_srcId: f.al_srcID,
-                            num_link: 1,
                             flight: [f.al_name]
                         };
                         airports[i].in_edges.push(edge_tmp);
-                        airports[i].in_edge_dic.push(f.al_des);
+                        airports[i].in_edge_dic.push(f.al_src);
                     }
 
                     // Existing edge, just need to update
                     else {
                         airports[i].in_edges.forEach(function (edge) {
                             if (edge.edge_src === f.src) {
-                                edge.num_link += 1;
                                 edge.flight.push(f.al_name);
                             }
                         });
@@ -151,6 +146,7 @@ function BuildData(flight, ap_supplement) {
             return a.id - b.id;
         }
     });
+
     // add extra airport information
     airports.forEach(function (airport) {
         var search_ap = ap_supplement.filter(function (s) {
@@ -159,23 +155,31 @@ function BuildData(flight, ap_supplement) {
         if(search_ap[0] !== undefined) {
             airport.name = search_ap[0].name;
             airport.country = search_ap[0].country;
-            airport.latitude = search_ap[0].latitude;
-            airport.longitude = search_ap[0].longitude;
-            airport.altitude = search_ap[0].altitude;
+            airport.latitude = +search_ap[0].latitude;
+            airport.longitude = +search_ap[0].longitude;
+            airport.altitude = +search_ap[0].altitude;
         } else {
-            airport.name = null;
-            airport.country = null;
-            airport.latitude = null;
-            airport.longitude = null;
-            airport.altitude = null;
+            for (var i = 0; i < all_ap.length; i++) {
+                if (airport.code === all_ap[i].gps_code ||
+                    airport.code === all_ap[i].iata_code ||
+                    airport.code === all_ap[i].local_code) {
+
+                    airport.name = all_ap[i].name;
+                    airport.country = all_ap[i].iso_country;
+                    airport.latitude = +all_ap[i].latitude_deg;
+                    airport.longitude = +all_ap[i].longitude_deg;
+                    airport.altitude = +all_ap[i].elevation_ft;
+                    break;
+                }
+            }
         }
     });
     return airports;
 }
 
-function low_level(selector, flight, ap_supplement) {
+function low_level(selector, flight, ap_supplement, all_ap) {
 
-    var airports = BuildData(flight, ap_supplement);
+    var airports = BuildData(flight, ap_supplement, all_ap);
     console.log('airport', airports);
 
     var height = 750,
@@ -188,21 +192,18 @@ function low_level(selector, flight, ap_supplement) {
         .attr('width', width)
         .attr('height', height);
 
-    var xAxisWidth = width - 20,
-        yAxisWidth = height - 20;
-
     var longitudeScale = d3.scaleLinear()
         .domain([-180.0, 180.0])
-        .range([0, xAxisWidth]);
+        .range([0, width - padding.left - padding.right]);
 
     var latitudeScale = d3.scaleLinear()
         .domain([-90, 90])
-        .range([yAxisWidth, 0]);
+        .range([height - padding.top - padding.bottom, 0]);
 
     airports.forEach(function (ap) {
         if (ap.latitude !== undefined && ap.longitude !== undefined) {
-            ap.x = longitudeScale(ap.longitude);
-            ap.y = latitudeScale(ap.latitude);
+            ap.x = +longitudeScale(ap.longitude);
+            ap.y = +latitudeScale(ap.latitude);
         } else {
             ap.x = 0;
             ap.y = 0;
@@ -214,7 +215,80 @@ function low_level(selector, flight, ap_supplement) {
         .attr('class', 'dot')
         .attr('cx', function (d) { return d.x; })
         .attr('cy', function (d) { return d.y; })
-        .attr('r', 2)
+        .attr('r', 2);
+
+    function rect(x, y, w, h) {
+        return "M"+[x,y]+" l"+[w,0]+" l"+[0,h]+" l"+[-w,0]+"z";
+    }
+
+    var selection = svg.append("path")
+        .attr("class", "selection")
+        .attr("visibility", "hidden");
+
+    var startSelection = function(start) {
+        selection.attr("d", rect(start[0], start[0], 0, 0))
+            .attr("visibility", "visible")
+            .attr('fill-opacity', '0')
+            .attr('stroke', 'red')
+            .attr('stroke-width', 3);
+    };
+
+    var moveSelection = function(start, moved) {
+        selection.attr("d", rect(start[0], start[1], moved[0]-start[0], moved[1]-start[1]));
+    };
+
+    var endSelection = function(start, end) {
+        //selection.attr("visibility", "hidden");
+    };
+
+    svg.on("mousedown", function() {
+        var subject = d3.select(window),
+            parent = this.parentNode,
+            start = d3.mouse(parent);
+        startSelection(start);
+        subject
+            .on("mousemove.selection", function() {
+                moveSelection(start, d3.mouse(parent));
+            })
+            .on("mouseup.selection", function() {
+            endSelection(start, d3.mouse(parent));
+            subject.on("mousemove.selection", null).on("mouseup.selection", null);
+        });
+    });
+
+    // Build links between nodes
+    var links = [],
+        len = airports.length;
+
+    airports.forEach(function (ap) {
+        var i = 0;
+        ap.out_edges.forEach(function (out_edge) {
+            for (i = 0; i < len; i++) {
+                if (airports[i].code === out_edge.edge_des) {
+                    out_edge.des_x = airports[i].x;
+                    out_edge.des_y = airports[i].y;
+                    break;
+                }
+            }
+
+            var x1 = out_edge.des_x, y1 = out_edge.des_y,
+                x2 = ap.x, y2 = ap.y;
+
+
+            links = svg.append('path')
+                .classed('link', true)
+                .attr('d', 'M '+ x2 +' '+ y2 +' '
+                        + 'Q' + ' ' + ((x1+x2+y2-y1)/2) + ' '
+                        + ((y1+y2+x1-x2)/2) +' '+ x1 + ' ' + y1)
+                .attr('stroke', 'red')
+                .attr('stroke-width', 1)
+                .attr('fill', 'none')
+                .attr('opacity', 0.3);
+            //console.log(links)
+
+        })
+    })
+
 
 
 
