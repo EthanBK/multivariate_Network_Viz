3eb45354d4c7ee158a2274a48e1841d0298d396e
Michael Yen <lvlichael8@gmail.com>
2018-03-08 04:48:58 -0800

    Add src/selection_component.js

---
+++ src/selection_component.js
@@ -0,0 +1,24 @@
+var SelectionComponentObj = new SelectionComponent()
+
+function SelectionComponent() {
+    var $selection_component = $('#selection_component');
+
+    this.Children = [];
+
+    this.addSelection = function(color) {
+        var child_count = Object.keys(this.Children).length;
+        var id = 'sc-' + child_count;
+
+        // Create the element
+        $selection_component.append('<div id="' + id + '"class="container-fluid sc-child"></div>');
+
+        // Retain a pointer to the element
+        var $child = $('#' + id);
+        this.Children.push({
+            id: id,
+            color: color,
+            ref: $child
+        });
+        $child.append('<div class="selection-color-box top-10" style="background:' + color + '"></div>')
+    }
+};
